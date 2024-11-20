3bae8948913ca9824819c9dc1466d87fca3a6376
floodx92 <floodx92@gmail.com>
2024-11-20 15:13:42 +0100

    Revert "Remove grammar replaces"
    This reverts commit 3903ff92b629b2bbd7542ec0d18615e6237c6bf5.

--- src/D1/D1SchemaGrammar.php
+++ src/D1/D1SchemaGrammar.php
@@ -10,36 +10,42 @@ class D1SchemaGrammar extends SQLiteGrammar
     public function compileTableExists($table): string
     {
         return Str::of(parent::compileTableExists($table))
+            ->replace('sqlite_master', 'sqlite_schema')
             ->__toString();
     }
 
     public function compileDropAllTables(): string
     {
         return Str::of(parent::compileDropAllTables())
+            ->replace('sqlite_master', 'sqlite_schema')
             ->__toString();
     }
 
     public function compileDropAllViews(): string
     {
         return Str::of(parent::compileDropAllViews())
+            ->replace('sqlite_master', 'sqlite_schema')
             ->__toString();
     }
 
     public function compileSqlCreateStatement($name, $type = 'table'): string
     {
         return Str::of(parent::compileSqlCreateStatement($name, $type))
+            ->replace('sqlite_master', 'sqlite_schema')
             ->__toString();
     }
 
     public function compileViews(): string
     {
         return Str::of(parent::compileViews())
+            ->replace('sqlite_master', 'sqlite_schema')
             ->__toString();
     }
 
     public function compileTables($withSize = false): string
     {
         return Str::of(parent::compileTables($withSize))
+            ->replace('sqlite_master', 'sqlite_schema')
             ->__toString();
     }
 }
