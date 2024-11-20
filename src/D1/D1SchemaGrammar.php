236d6886f913d7d16342a303fac6fae5f62f958b
floodx92 <floodx92@gmail.com>
2024-11-20 16:06:27 +0100

    Fix cf table selecting

--- src/D1/D1SchemaGrammar.php
+++ src/D1/D1SchemaGrammar.php
@@ -27,7 +27,7 @@ class D1SchemaGrammar extends SQLiteGrammar
      */
     public function compileDropAllTables(): string
     {
-        return "SELECT 'DROP TABLE IF EXISTS \"' || name || '\";' AS drop_statement FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%'";
+        return "SELECT 'DROP TABLE IF EXISTS \"' || name || '\";' AS drop_statement FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE '_cf%'";
     }
 
     /**
@@ -37,7 +37,7 @@ class D1SchemaGrammar extends SQLiteGrammar
      */
     public function compileDropAllViews(): string
     {
-        return "SELECT 'DROP VIEW IF EXISTS \"' || name || '\";' AS drop_statement FROM sqlite_schema WHERE type = 'view' AND name NOT LIKE 'sqlite_%'";
+        return "SELECT 'DROP VIEW IF EXISTS \"' || name || '\";' AS drop_statement FROM sqlite_schema WHERE type = 'view' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE '_cf%'";
     }
 
     /**
